8e46a43d3505b86e857939f863fef26288c035bd
Joel S <joel.devbox@protonmail.com>
2020-04-04 20:43:22 +0530

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -8,10 +8,8 @@ breakfast $DEVICE
 export USE_HOST_LEX=yes
 make -j$(nproc) mkbootimg
 make -j$(nproc) halium-boot
-make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum $ANDROID_ROOT/out/target/product/mido/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/mido/recovery.img
 md5sum $ANDROID_ROOT/out/target/product/mido/system.img
