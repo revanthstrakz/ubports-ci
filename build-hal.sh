db861877381c773cbb7d6d809ab8d54e6dfe9da6
Joel S <joel.devbox@protonmail.com>
2020-04-04 13:37:08 +0530

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -5,11 +5,12 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
+export USE_HOST_LEX=yes
 make -j$(nproc) halium-boot
 make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/santoni/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/system.img
+md5sum $ANDROID_ROOT/out/target/product/mido/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/mido/recovery.img
+md5sum $ANDROID_ROOT/out/target/product/mido/system.img
